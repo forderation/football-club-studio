d667667512ff6a1789e5b20059469b29a59fed05
forderation <kharisma.muzaki@gmail.com>
2019-11-05 20:06:22 +0700

    init submission 1 config complete

---
+++ app/src/main/java/com/forderation/footballclubstudio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.forderation.footballclubstudio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
