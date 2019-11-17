0b6b2aea4a0608b4b72b3a44ce5530d2feaf9aa6
forderation <kharisma.muzaki@gmail.com>
2019-11-18 02:14:25 +0700

    frame search and detail event

--- app/src/main/java/com/forderation/footballclubstudio/activity/EventDetailActivity.kt
+++ app/src/main/java/com/forderation/footballclubstudio/activity/EventDetailActivity.kt
@@ -2,11 +2,12 @@ package com.forderation.footballclubstudio.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.forderation.footballclubstudio.R
 
-class ClubDetailActivity : AppCompatActivity() {
+class EventDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_event_detail)
     }
 }
